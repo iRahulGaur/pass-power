c048db388f0a84e8e3b690abfb9e40cbdc8df224
Rahul Gaur <rahul.gaur152@gmail.com>
2020-03-05 17:25:50 +0530

    working on lock press to show custom popUp instead of power popUp

--- Android/app/src/main/java/com/rahulgaur/myapplication/MainActivity.kt
+++ Android/app/src/main/java/com/rahulgaur/myapplication/MainActivity.kt
@@ -1,12 +1,60 @@
 package com.rahulgaur.myapplication
 
+import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.Window
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var context: Context
+    private var isPass: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        context = this
+
+    }
+
+    override fun onKeyLongPress(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_POWER) {
+            if (!isPass) {
+                val dialog = Dialog(this)
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+                dialog.setCancelable(false)
+                dialog.setContentView(R.layout.layout_passcode_dialog)
+
+                val editText = dialog.findViewById<TextView>(R.id.passCodeET)
+                val btn = dialog.findViewById<TextView>(R.id.passCodeBtn)
+
+                val correctPass = "2222"
+
+                btn.setOnClickListener {
+
+                    val passCode = editText.text.toString()
+
+                    if (passCode == correctPass) {
+                        Toast.makeText(this, "Password is correct", Toast.LENGTH_SHORT).show()
+                        isPass = true
+                    } else {
+                        Toast.makeText(this, "Password is incorrect", Toast.LENGTH_SHORT).show()
+                    }
+                    dialog.show()
+                }
+                return false
+            } else if (isPass) {
+                return super.onKeyLongPress(keyCode, event)
+            } else {
+                return true
+            }
+        } else {
+            return super.onKeyLongPress(keyCode, event)
+        }
     }
 }
---
+++ Android/app/src/main/java/com/rahulgaur/myapplication/MyApplication.kt
@@ -0,0 +1,9 @@
+package com.rahulgaur.myapplication
+
+import android.app.Application
+
+/**
+ * Created by Rahul Gaur on 05,March,2020
+ * Email: rahul@appwebstudios.com
+ */
+class MyApplication : Application()
